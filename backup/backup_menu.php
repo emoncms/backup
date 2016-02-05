613ad096c9f20c5a6db75091e58ad8d9940c8b09
Glyn Hudson <glyn.hudson@gmail.com>
2016-02-05 16:49:22 +0000

    set backup icon

--- backup/backup_menu.php
+++ backup/backup_menu.php
@@ -1,3 +1,3 @@
 <?php
 
-    $menu_dropdown_config[] = array('name'=> dgettext($domain, "Backup"), 'icon'=>'icon-tags', 'path'=>"backup/view" , 'session'=>"write", 'order' => 9 );
+    $menu_dropdown_config[] = array('name'=> dgettext($domain, "Backup"), 'icon'=>'icon-circle-arrow-down', 'path'=>"backup/view" , 'session'=>"write", 'order' => 9 );
