acbd5f66cb09fa20c9c6d1921ca97287df1ac06f
Glyn <glyn.hudson@gmail.com>
2016-02-05 16:46:48 +0000

    export mysql based on username and password from settings.php

--- get_emoncms_mysql_auth.php
+++ get_emoncms_mysql_auth.php
@@ -1,9 +1,9 @@
 <?php
 
-    file_put_contents("/tmp/checkuser", "test");
-    $user = fileowner("/tmp/checkuser");
-    unlink("/tmp/checkuser");
-    if ($user!=1000) die;
+    // file_put_contents("/tmp/checkuser", "test");
+    // $user = fileowner("/tmp/checkuser");
+    // unlink("/tmp/checkuser");
+    // if ($user!=1000) die;
 
     chdir("/var/www/emoncms");
     define('EMONCMS_EXEC', 1);
