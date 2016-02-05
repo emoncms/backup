0baacab36130d0478a48a3164b359b5707c9fb6a
Glyn Hudson <glyn.hudson@gmail.com>
2016-02-05 16:38:38 +0000

    add support for datestamp in backup filename & SD card image version checking and restore correct conf acordingly

--- emoncms-export.sh
+++ emoncms-export.sh
@@ -1,10 +1,52 @@
 #!/bin/bash
+data_path="/home/pi/data"
 
-cd /home/pi/data
+date=$(date +"%Y-%m-%d")
+
+echo "Emoncms export...from $data_path"
+echo $date
+
+#-----------------------------------------------------------------------------------------------
+# Check emonPi / emonBase image version
+#-----------------------------------------------------------------------------------------------
+image_version=$(ls /boot | grep emonSD)
+# Check first 16 characters of filename
+image_date=${image_version:0:16}
+if [[ "$image_date" == "emonSD-17Jun2015" ]]
+then
+  image="old"
+  echo "$image image"
+else
+  image="new"
+  echo "$image image"
+fi
+
+if [[ "${image_version:0:6}" == "emonSD" ]]
+then
+    echo "Image version: $image_version"
+else
+    echo "Non OpenEnergyMonitor offical emonSD image, no gurantees this script will work :-/"
+    read -p "Press any key to continue...or CTRL+C to exit " -n1 -s
+fi
+#-----------------------------------------------------------------------------------------------
+
+cd $data_path
 
 sudo service feedwriter stop
 
-mysqldump -u root -praspberry emoncms > /home/pi/data/emoncms.sql
-tar -cvzf backup.tar.gz /home/pi/data/emoncms.sql /home/pi/data/phpfina /home/pi/data/phptimeseries /home/pi/data/emonhub.conf /home/pi/data/emoncms.conf
+# MYSQL Dump Emoncms database
+if [[ $image == "old" ]]
+then
+  mysqldump -u root -praspberry emoncms > $data_path/emoncms.sql
+else
+  mysqldump -u emoncms -pemonpiemoncmsmysql2016 emoncms > $data_path/emoncms.sql
+fi
+
+# Compress backup with database and config files
+tar -cvzf backup-$date.tar.gz emoncms.sql phpfina phptimeseries emonhub.conf emoncms.conf
 
 sudo service feedwriter start
+
+echo "backup saved $data_path/backup-$date.tar.gz"
+echo "done"
+date
--- emoncms-import.sh
+++ emoncms-import.sh
@@ -1,23 +1,97 @@
 #!/bin/bash
 
-sudo service emonhub stop
-sudo service emoncms-nodes-service stop
-sudo service feedwriter stop
+backup_source_path="/home/pi/data/uploads"
+data_path="/home/pi/data"
 
-sudo tar xvfz backup.tar.gz -C /
-mysql -u root -praspberry emoncms < /home/pi/data/emoncms.sql
+echo "Emoncms import...from $backup_source_path"
+echo "...to $data_path"
+date
 
-mv /home/pi/data/emonhub.conf /home/pi/data/old.emonhub.conf
-cp /home/pi/emonhub/conf/emonhub.conf /home/pi/data/emonhub.conf
 
-mv /home/pi/data/emoncms.conf /home/pi/data/old.emoncms.conf
+#-----------------------------------------------------------------------------------------------
+# Check emonPi / emonBase image version
+#-----------------------------------------------------------------------------------------------
+image_version=$(ls /boot | grep emonSD)
+# Check first 16 characters of filename
+image_date=${image_version:0:16}
+if [[ "$image_date" == "emonSD-17Jun2015" ]]
+then
+  image="old"
+  echo "$image image"
+else
+  image="new"
+  echo "$image image"
+fi
 
-sudo touch /home/pi/data/emoncms.conf
-sudo chown pi:www-data /home/pi/data/emoncms.conf
-sudo chmod 664 /home/pi/data/emoncms.conf
+if [[ "${image_version:0:6}" == "emonSD" ]]
+then
+    echo "Image version: $image_version"
+else
+    echo "Non OpenEnergyMonitor offical emonSD image, no gurantees this import will work :-/"
+    read -p "Press any key to continue...or CTRL+C to exit " -n1 -s
+fi
+#-----------------------------------------------------------------------------------------------
+
+
+
+# Get latest backup filename
+backup_filename=$((cd $backup_source_path && ls -t *.gz) | grep backup | head -1)
+cd ~/
+if [[ -z "$backup_filename" ]] #if backup does not exist (empty filename string)
+then
+    echo "backup does not exit..stoppping import"
+    exit 1
+else # if backup exists
+  echo "backup found: $backup_filename starting import.."
+
+  rpi-rw
+  sudo service emonhub stop
+  sudo service emoncms-nodes-service stop
+  sudo service feedwriter stop
+
+  # Uncompress backup
+  tar xvfz $backup_source_path/$backup_filename -C /
+
+  # Restore Emoncms MYSQL database
+  if [[ $image == "old" ]]
+  then
+    mysql -u root -praspberry emoncms < $data_path/emoncms.sql
+  else
+    mysql -u emoncms -pemonpiemoncmsmysql2016 emoncms <$data_path/emoncms.sql
+  fi
+
+fi
+
+
+# Restore settings saving as old.xxxxx
+echo "backup settings emonhub.conf > $data_path/old.emohub.conf"
+mv $data_path/emonhub.conf $data_path/old.emonhub.conf
+mv $data_path/emoncms.conf $data_path/old.emoncms.conf
+
+
+# Start with blank emonhub.conf
+if [[ $image == "old" ]]
+then    # Legacy image use emonhub.conf without MQTT authenitication
+   echo "Start with fresh config: copy LEGACY default emonhub.conf"
+   echo "/home/pi/emonhub/conf/old.default.emonhub.conf /home/pi/data/emonhub.conf"
+   cp /home/pi/emonhub/conf/old.default.emonhub.conf $data_path/emonhub.conf
+else    # Newer Feb15+ image use latest emonhub.conf with MQTT node variable topic structure and MQTT authentication enabled
+   echo "Start with fresh config: copy NEW default emonpi emonhub.conf"
+   echo "cp /home/pi/emonhub/conf/emonpi.default.emonhub.conf /home/pi/data/emonhub.conf"
+   cp /home/pi/emonhub/conf/emonpi.default.emonhub.conf $data_path/emonhub.conf
+fi
+
+# Create blank emoncms.conf and ensure permissions are correct
+sudo touch $data_path/emoncms.conf
+sudo chown pi:www-data $data_path/emoncms.conf
+sudo chmod 664 $data_path/emoncms.conf
 
 redis-cli "flushall"
 
 sudo service emonhub start
 sudo service emoncms-nodes-service start
 sudo service feedwriter start
+
+echo "done"
+date
+rpi-rw
