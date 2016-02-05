eed45337243d5e26e899740c131aa1379ba25d2b
Glyn <glyn.hudson@gmail.com>
2016-02-05 17:50:25 +0000

    correction to view location

--- backup/backup_menu.php
+++ backup/backup_menu.php
@@ -1,3 +1,5 @@
 <?php
-
-    $menu_dropdown_config[] = array('name'=> dgettext($domain, "Backup"), 'icon'=>'icon-circle-arrow-down', 'path'=>"backup/view" , 'session'=>"write", 'order' => 36 );
+    $domain = "messages";
+    bindtextdomain($domain, "Modules/admin/locale");
+    bind_textdomain_codeset($domain, 'UTF-8');
+    $menu_dropdown_config[] = array('name'=> dgettext($domain, "Backup"), 'icon'=>'icon-circle-arrow-down', 'path'=>"backup" , 'session'=>"write", 'order' => 36 );
