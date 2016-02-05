82074b641d4b42730388a2ae46d9450aef0ba0d2
Glyn <glyn.hudson@gmail.com>
2016-02-05 16:30:07 +0000

    php script to access mysql settings for use with import and export bash scripts

---
+++ get_emoncms_mysql_auth.php
@@ -0,0 +1,11 @@
+<?php
+
+    file_put_contents("/tmp/checkuser", "test");
+    $user = fileowner("/tmp/checkuser");
+    unlink("/tmp/checkuser");
+    if ($user!=1000) die;
+
+    chdir("/var/www/emoncms");
+    define('EMONCMS_EXEC', 1);
+    require "process_settings.php";
+    echo $username.":".$password;
