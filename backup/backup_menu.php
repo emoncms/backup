9d85727f9ee119cc647d6933319b1dd23d6c3ba3
Glyn Hudson <glyn.hudson@gmail.com>
2016-02-09 15:37:46 +0000

    move menue item to bottom

--- backup/backup_menu.php
+++ backup/backup_menu.php
@@ -2,4 +2,4 @@
     $domain = "messages";
     bindtextdomain($domain, "Modules/admin/locale");
     bind_textdomain_codeset($domain, 'UTF-8');
-    $menu_dropdown_config[] = array('name'=> dgettext($domain, "Backup"), 'icon'=>'icon-circle-arrow-down', 'path'=>"backup" , 'session'=>"write", 'order' => 36 );
+    $menu_dropdown_config[] = array('name'=> dgettext($domain, "Backup"), 'icon'=>'icon-circle-arrow-down', 'path'=>"backup" , 'session'=>"write", 'order' => 55 );
