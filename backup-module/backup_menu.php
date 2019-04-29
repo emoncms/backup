527ff9a5b16d8ac39459c9fd433103cc600b034e
Emrys <emrys@openenergymonitor.org>
2019-04-29 13:00:51 +0100

    edited menu to remove old menu style

--- backup-module/backup_menu.php
+++ backup-module/backup_menu.php
@@ -1,26 +1,26 @@
 <?php
-    $domain = "messages";
-    bindtextdomain($domain, "Modules/admin/locale");
-    bind_textdomain_codeset($domain, 'UTF-8');
+    // $domain = "messages";
+    // bindtextdomain($domain, "Modules/admin/locale");
+    // bind_textdomain_codeset($domain, 'UTF-8');
     
-    $menu_left[] = array(
-        'id'=>"backup_menu",
-        'name'=>dgettext($domain, "Backup"), 
-        'path'=>"backup" , 
-        'session'=>"write", 
-        'order' => 0,
-        'icon'=>'icon-circle-arrow-down icon-white',
-        'hideinactive'=>1
-    );
+    // $menu_left[] = array(
+    //     'id'=>"backup_menu",
+    //     'name'=>dgettext($domain, "Backup"), 
+    //     'path'=>"backup" , 
+    //     'session'=>"write", 
+    //     'order' => 0,
+    //     'icon'=>'icon-circle-arrow-down icon-white',
+    //     'hideinactive'=>1
+    // );
 
-    $menu_dropdown_config[] = array(
-        'id'=>"backup_menu_extras",
-        'name'=>dgettext($domain, "Backup"), 
-        'path'=>"backup" , 
-        'session'=>"write", 
-        'order' => 55,
-        'icon'=>'icon-circle-arrow-down'
-    );
+    // $menu_dropdown_config[] = array(
+    //     'id'=>"backup_menu_extras",
+    //     'name'=>dgettext($domain, "Backup"), 
+    //     'path'=>"backup" , 
+    //     'session'=>"write", 
+    //     'order' => 55,
+    //     'icon'=>'icon-circle-arrow-down'
+    // );
     
     $menu['setup'][] = array(
         'text' => _("Backup"),
@@ -28,5 +28,3 @@
         'icon' => 'box-add',
         'order' => 1
     );
-
-
