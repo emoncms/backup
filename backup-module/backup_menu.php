490cdf2da2f788570ac275c5baf65ab11661b085
TrystanLea <glyn.hudson@openenergymonitor.org>
2019-08-11 21:27:46 +0100

    move backup to emoncms setup menu

--- backup-module/backup_menu.php
+++ backup-module/backup_menu.php
@@ -22,11 +22,11 @@
     //     'icon'=>'icon-circle-arrow-down'
     // );
     
-    $menu['setup'][] = array(
+    $menu['sidebar']['emoncms'][] = array(
         'text' => _("Backup"),
         'path' => 'backup',
         'icon' => 'box-add',
-        'order' => 1
+        'order' => 'b5'
     );
 
 
