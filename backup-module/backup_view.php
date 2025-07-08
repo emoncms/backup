cd00a746c75d68db8cf9f802f6d1bc6f2f58b155
Alexandre CUER <alexandrep.cuer@gmail.com>
2025-07-08 17:58:24 +0200

    view and controler use json locale
    Add french locale

--- backup-module/backup_view.php
+++ backup-module/backup_view.php
@@ -18,70 +18,78 @@
 
 
 <ul class="nav nav-tabs mb-0 mt-3" id="backup-tabs">
-    <li class="active"><a href="#view-import-usb">Import USB</a></li>
-    <li><a href="#view-import-archive">Import Archive</a></li>
-    <li><a href="#view-export">Export</a></li>
+    <li class="active"><a href="#view-import-usb"><?php echo tr("Import USB"); ?></a></li>
+    <li><a href="#view-import-archive"><?php echo tr("Import Archive"); ?></a></li>
+    <li><a href="#view-export"><?php echo tr("Export Archive"); ?></a></li>
 </ul>
     
 <div class="tab-content">
     <div class="tab-pane active" id="view-import-usb">
-        <h3>Import from USB drive</h3>
-        <p>Import emoncms account data from old emonSD card mounted as a USB drive.</p>
-        <p>Place your old emonPi or emonBase SD card in a USB SD card reader and plug into one of the raspberry pi USB ports.<br>This importer will then find and import all emoncms account data without the need to export and import an archive.</p>
-        <span style="color:red;font-weight:bold;">CAUTION ALL EMONCMS ACCOUNT DATA WILL BE OVERWRITTEN BY THE IMPORTED DATA</span><br><br>
-        <p><i>Note: Before import update to latest version of Emoncms & emonHub.</i></p>
-        <button id="usb-import" class="btn btn-danger"><?php echo _('Import from USB drive'); ?></button>
+        <h3><?php echo tr("Import from USB drive"); ?></h3>
+        <p><?php echo tr("Import emoncms account data from old emonSD card mounted as a USB drive."); ?></p>
+        <p><?php echo tr("Place your old emonPi or emonBase SD card in a USB SD card reader and plug into one of the raspberry pi USB ports.<br>This importer will then find and import all emoncms account data without the need to export and import an archive."); ?></p>
+        <span style="color:red;font-weight:bold;">
+          <?php echo tr("CAUTION ALL EMONCMS ACCOUNT DATA WILL BE OVERWRITTEN BY THE IMPORTED DATA"); ?>
+        </span><br><br>
+        <p><i><?php echo tr("Note: Before import update to latest version of Emoncms & EmonHub."); ?></i></p>
+        <button id="usb-import" class="btn btn-danger"><?php echo tr('Import from USB drive'); ?></button>
         <br><br>
         <pre id="usb-import-log-bound" class="log"><div id="usb-import-log"></div></pre>
         <br>
-        <p><i>Refresh page if log window does not update.</i></p>
-        <p><i>After import is complete logout then login using the new imported account login details.</i></p>
+        <p><i><?php echo tr("Refresh page if log window does not update."); ?></i></p>
+        <p><i><?php echo tr("After import is complete logout then login using the new imported account login details."); ?></i></p>
     </div>
     <div class="tab-pane" id="view-import-archive">
-        <h3>Import from Archive</h3>
-        <p>Import an emoncms backup archive.</p>
-        <span style="color:red;font-weight:bold;">CAUTION ALL EMONCMS ACCOUNT DATA WILL BE OVERWRITTEN BY THE IMPORTED DATA</span><br><br>
-        <p><i>Note: Before import update to latest version of Emoncms & emonHub.</i></p>
+        <h3><?php echo tr("Import from Archive"); ?></h3>
+        <p><?php echo tr("Import an emoncms backup archive."); ?></p>
+        <span style="color:red;font-weight:bold;">
+            <?php echo tr("CAUTION ALL EMONCMS ACCOUNT DATA WILL BE OVERWRITTEN BY THE IMPORTED DATA"); ?>
+        </span><br><br>
+        <p><i><?php echo tr("Note: Before import update to latest version of Emoncms & EmonHub."); ?></i></p>
         <form action="<?php echo $path; ?>backup/upload" method="post" enctype="multipart/form-data">
         <input type="file" name="file" id="file"><br><br>
-        <input class="btn btn-danger" type="submit" name="submit" value="Import Backup">
+        <input class="btn btn-danger" type="submit" name="submit" value="<?php echo tr("Import Backup"); ?>">
         </form>
         <br><br>
-        <p><i>Note: If browser upload fails for large backup files <a href="http://github.com/emoncms/backup">follow manual import instructions.</a></i></p>
+        <p><i>
+            <?php echo tr("Note: If browser upload fails for large backup files"); ?> 
+            <a href="http://github.com/emoncms/backup"><?php echo tr("follow manual import instructions."); ?></a>
+        </i></p>
         <pre id="import-log-bound" class="log"><div id="import-log"></div></pre>
         <br>
-        <p><i>Refresh page if log window does not update.</i></p>
-        <p><i>After import is complete logout then login using the new imported account login details.</i></p>
+        <p><i><?php echo tr("Refresh page if log window does not update."); ?></i></p>
+        <p><i><?php echo tr("After import is complete logout then login using the new imported account login details."); ?></i></p>
     </div>
     <div class="tab-pane" id="view-export">
-        <h3>Export</h3>
-        <p>Export a compressed archive containing:</p>
+        <h3><?php echo tr("Export"); ?></h3>
+        <p><?php echo tr("Export a compressed archive containing:"); ?></p>
         <ul>
-        <li>Emoncms MYSQL database</li>
-        <li>PHPFina data files</li>
-        <li>PHPTimeSeries data files</li>
-        <li>EmonHub Config</li>
+        <li><?php echo tr("Emoncms MYSQL database"); ?></li>
+        <li><?php echo tr("PHPFina data files"); ?></li>
+        <li><?php echo tr("PHPTimeSeries data files"); ?></li>
+        <li><?php echo tr("EmonHub Config"); ?></li>
         </ul>
-        <p>These files contain all Emoncms data including:</p>
+        <p><?php echo tr("These files contain all Emoncms data including:"); ?></p>
         <ul>
-        <li>Input processes</li>
-        <li>Feed data</li>
-        <li>Dashboards</li>
-        <li>EmonHub Config</li>
+        <li><?php echo tr("Input processes"); ?></li>
+        <li><?php echo tr("Feed data"); ?></li>
+        <li><?php echo tr("Dashboards"); ?></li>
+        <li><?php echo tr("EmonHub Config"); ?></li>
         </ul>
-        <p>The compressed archive can be used to migrate data to another emonPi / emonBase.</p>
-        <button id="emonpi-backup" class="btn btn-info"><?php echo _('Create backup'); ?></button>
+        <p><?php echo tr("The compressed archive can be used to migrate data to another emonPi / emonBase."); ?></p>
+        <button id="emonpi-backup" class="btn btn-info"><?php echo tr('Create backup'); ?></button>
         <br><br>
         <pre id="export-log-bound" class="log"><div id="export-log"></div></pre>
         <?php
         $backup_filename="emoncms-backup-".gethostname()."-".date("Y-m-d").".tar.gz";
         if (file_exists($parsed_ini['backup_location']."/".$backup_filename) && !file_exists("/tmp/backuplock")) {
-            echo '<br><br><b>Right Click > Download:</b><br><a href="'.$path.'backup/download">'.$backup_filename.'</a>';
+            echo '<br><br><b>'.tr("Right Click > Download:");
+            echo '</b><br><a href="'.$path.'backup/download">'.$backup_filename.'</a>';
         }
         ?>
         <br><br>
-        <p>Once export is complete refresh page to see download link.</p>
-        <p><i>Note: Export can take a long time; please be patient.</i></p>
+        <p><?php echo tr("Once export is complete refresh page to see download link."); ?></p>
+        <p><i><?php echo tr("Note: Export can take a long time; please be patient."); ?></i></p>
     </div>
 </div>
 
@@ -123,7 +131,10 @@
 
   <?php
     if (empty($servicerunnerproc)) {
-        echo "<div class='alert alert-error'><b>Warning:</b> service-runner is not running and is required. To install service-runner see <a href='https://github.com/emoncms/emoncms/blob/master/scripts/services/install-service-runner-update.md'>service-runner installation</a></div>";
+        echo "<div class='alert alert-error'><b>".tr("Warning:");
+        echo "</b> ".tr("service-runner is not running and is required. To install service-runner see");
+        echo " <a href='https://github.com/emoncms/emoncms/blob/master/scripts/services/install-service-runner-update.md'>";
+        echo tr("service-runner installation")."</a></div>";
     }
   ?>
 
